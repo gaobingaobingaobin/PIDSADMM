206362a453b1fabe144e42f1d408f91e5516e193
gaobingaobingaobin <garbagegao@126.com>
2016-09-08 15:12:24 +0800

    Update SimulationTable1.m

--- SimulationTable1.m
+++ SimulationTable1.m
@@ -1,38 +1,40 @@
-% Total variation denoising with random data
+% L1-regularized least-squares example
 %% Generate problem data
 
-rand('seed', 0);
 randn('seed', 0);
+rand('seed',0);
 
-% n = 100;
-       % number of examples
-nn = 100;       % number of features
+mm = 150;       % number of examples
+nn = 500;       % number of features
 % pp = 100/n;      % sparsity density
-dimension=10;
+dimension=5;
 for ii=1:dimension
-    n=nn+(ii-1)*100;
+    m=mm+(ii-1)*150+750;
+    mmm(ii)=m;
+    n=nn+(ii-1)*500+2500;
     nnn(ii)=n;
-x0 = ones(n,1);
-for j = 1:3
-    idx = randsample(n,1);
-    k = randsample(1:10,1);
-    x0(ceil(idx/2):idx) = k*x0(ceil(idx/2):idx);
-end
-b = x0 + randn(n,1);
-
-lambda = 5;
-
-e = ones(n,1);
-D = spdiags([e -e], 0:1, n,n);
-DTD=D'*D;
-[v,d]=eigs(DTD);
-r=max(d(:));
-
-
-  alpha=-0.1;
+    p=100/n;
+    x0 = sprandn(n,1,p);
+    A = randn(m,n);
+    A = A*spdiags(1./sqrt(sum(A.^2))',0,n,n); % normalize columns
+    % % save A
+    % load A
+    b = A*x0 + sqrt(0.001)*randn(m,1);
+    % save b
+    % load b
+    ATA=A'*A;
+    [v,d]=eigs(ATA);
+    r=max(d(:));
+    % save r %大约是7.9
+    % load r
+    lambda_max = norm( A'*b, 'inf' );
+    lambda = 0.1*lambda_max;
+    %% Solve problem
+    % tau=0.8;
+    alpha=-0.3;
     % [x1 history1] = lassoLADMM(A, b, lambda, 1.0,r);
-    [x3 history3] = total_variationILSADMM(b, lambda, 1.0,r,alpha);
-    [x2 history2] = total_variationLSADMM(b, lambda, 1.0,r,alpha);
+    [x3 history3] = lassoILSADMM(A, b, lambda, 1.0,r,alpha);
+    [x2 history2] = lassoLSADMM(A, b, lambda, 1.0,r,alpha);
     % kk1(ii)=history1.iteration;
     % tt1(ii)=history1.time;
     kk2(ii)=history2.iteration;
@@ -40,21 +42,20 @@ r=max(d(:));
     kk3(ii)=history3.iteration;
     tt3(ii)=history3.time;
 
-    alpha=0.1;
+    alpha=0.3;
     % [x11 history11] = lassoLADMM(A, b, lambda, 1.0,r);
-    [x33 history33] = total_variationILSADMM(b, lambda, 1.0,r,alpha);
-    [x22 history22] = total_variationLSADMM(b, lambda, 1.0,r,alpha);
+    [x33 history33] = lassoILSADMM(A, b, lambda, 1.0,r,alpha);
+    [x22 history22] = lassoLSADMM(A, b, lambda, 1.0,r,alpha);
     % kk11(ii)=history11.iteration;
     % tt11(ii)=history11.time;
     kk22(ii)=history22.iteration;
     tt22(ii)=history22.time;
     kk33(ii)=history33.iteration;
     tt33(ii)=history33.time;
-    fprintf('$  %3d$ & %3d(%10.2f)&%3d(%10.2f)&%10.2f(%10.2f)&%3d(%10.2f)&%3d(%10.2f)&%10.2f(%10.2f)\\\\ \n',...
-        nnn(ii),...   % n
+    fprintf('$ %3d \\times %3d$ & %3d(%10.2f)&%3d(%10.2f)&%10.2f(%10.2f)&%3d(%10.2f)&%3d(%10.2f)&%10.2f(%10.2f)\\\\ \n',...
+        mmm(ii),nnn(ii),...   %m * n
         kk2(ii), tt2(ii),kk3(ii), tt3(ii), kk3(ii)/kk2(ii),tt3(ii)/tt2(ii),...
         kk22(ii), tt22(ii),kk33(ii), tt33(ii), kk33(ii)/kk22(ii),tt33(ii)/tt22(ii));
-    clear D
-    clear DTD
+    clear A 
     clear b
 end
