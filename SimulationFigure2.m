3da046c1323de76c01acf8067e7466b56df1c6cd
gaobingaobingaobin <garbagegao@126.com>
2017-04-22 13:41:05 +0800

    Update SimulationFigure2.m

--- SimulationFigure2.m
+++ SimulationFigure2.m
@@ -7,13 +7,22 @@ close all
 %% TV data which extracted from simulationTable2.m
 tempY{1}=[214,185;260,209;231,206;57,44;121,106];
 tempY{2}=[176,154;213,174;189,171;47,37;99,88];
-for i=1:2% 不同的tolerance 
-subplot(1,2,i)
-
-bar(tempY{i},1)
-
+for i=1:2% 涓嶅悓鐨則olerance 
+% subplot(1,2,i)
+ figure(i);
+b=bar(tempY{i},1);
+hold on;
+    color=[0 0 0.75;0 1 0];
+    for j=1:2
+        set(b(j),'FaceColor',color(j,:));
+    end
 xlabel('Dimension of $D$','Interpreter','latex','fontsize',12);
 ylabel('Iteration Numbers')
-legend('PD-SADMM','PID-SADMM')
+legend('P-SADMM','ID-SADMM')
+  if i==1
+        title('$\alpha=-0.1$','Interpreter','latex','fontsize',12);
+    else
+        title('$\alpha=0.1$','Interpreter','latex','fontsize',12);
+    end
 set(gca,'XTickLabel',{'100*100','200*200','300*300','400*400','500*500'})
 end
